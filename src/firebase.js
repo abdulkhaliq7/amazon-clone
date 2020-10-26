72f3076438187ccd8873ea78f8a95316199b5f83
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2020-10-26 14:21:56 +0800

    Header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqeX3Uugx-ojOr0iE3g9r50gDo8HYzti8",
+    authDomain: "clone-17115.firebaseapp.com",
+    databaseURL: "https://clone-17115.firebaseio.com",
+    projectId: "clone-17115",
+    storageBucket: "clone-17115.appspot.com",
+    messagingSenderId: "584166458495",
+    appId: "1:584166458495:web:66b3a122aa23603f62eb4a"
+  };
+
+  
